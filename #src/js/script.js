06bdb45117d4c28d75867010b2f2c1bbe68ab01b
Tatyana <tanyshechka14121986@gmail.com>
2021-05-09 19:35:38 +0300

    fix: add pattern into phone input

--- #src/js/script.js
+++ #src/js/script.js
@@ -67,8 +67,3 @@ function rollSlider() {
 }
 
 // form
-function addSpaces(initial) {
-  initial.replace("/([0-9]{3})/", "\1 ");
-  initial.replace("/[0-9]{3} ([0-9]{3})/", "\1 ");
-  return initial;
-}
