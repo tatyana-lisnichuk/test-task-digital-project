1ea30d9a55a898f696c799d9cabfb707420623c7
Tatyana <tanyshechka14121986@gmail.com>
2021-05-11 10:19:09 +0300

    feat: add animations

--- #src/js/script.js
+++ #src/js/script.js
@@ -64,3 +64,45 @@ document.querySelector('.next-btn').addEventListener('click', function () {
 function rollSlider() {
   slider.style.transform = 'translate(-' + count * width + 'px)';
 }
+
+// Animation
+const animItems = document.querySelectorAll('._anim-items');
+
+
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  // animOnScroll();
+  // offset();
+function animOnScroll(params) {
+  for (let index = 0; index < animItems.length; index++) {
+    const animItem = animItems[index];
+    const animItemHeight = animItem.offsetHeight;
+    const animItemOffset = offset(animItem).top;
+    const animStart = 4;
+
+    let animItemPoint = window.innerHeight - animItemHeight / animStart;
+    if (animItemHeight > window.innerHeight) {
+      animItemPoint = window.innerHeight - window.innerHeight / animStart;
+    }
+    if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+      animItem.classList.add('_active');
+    } else {
+      animItem.classList.remove('_active');
+    }
+  }
+}
+function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return {
+    top: rect.top + scrollTop, left: rect.left + scrollLeft
+  };
+}
+
+
+
+
+  animOnScroll();
+}
